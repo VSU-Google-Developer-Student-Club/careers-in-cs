25988cc0d46e6f88f252279ecf68f7700975b6f9
Isaiah Freeman <104329626+KingTechnician@users.noreply.github.com>
2024-02-20 22:47:37 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,255 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Grid, Typography, Box, Modal, Button, Divider,Accordion,AccordionDetails,AccordionSummary,Paper, TextField,AppBar,Toolbar} from '@mui/material';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import {roles} from './rolesData';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import QRCode from 'react-qr-code';
+import CircularProgress from '@mui/material/CircularProgress';
+// Flutter, React Native, 
+
+
+const jobTitles = [
+  "Mobile Development",
+  "Security Analysis",
+  "Frontend Development",
+  "Backend Development",
+  "Game Design",
+  "Full Stack Development",
+  "Graphics Programming",
+  "Security Analysis",
+  "Ethical Hacking",
+  "Penetration Testing",
+  "Computer Vision",
+  "Natural Language Processing",
+  "Reinforcement Learning",
+  "IT Consulting",
+  "Project Management",
+  "Scrum",
+  "Solutions Architect",
+  "Blockchain",
+  "Quantitative Analysis"
+]
+
+const themeOptions ={
+  palette: {
+    mode:'dark',
+    type: 'dark',
+    primary: {
+      main: '#d26410',
+    },
+    secondary: {
+      main: '#8794e0',
+    },
+    resize:{
+      fontSize:50
+    }
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          // Target the input element for font size
+          '& .MuiInputBase-input': {
+            fontSize: '20px',
+          },
+          // Target the label for font size
+          '& .MuiInputLabel-root': {
+            fontSize: '20px',
+          },
+        },
+      },
+    },
+  },
+};
+
+
+
+function MoreInformationModal(props)
+{
+  React.useEffect(()=>
+  {
+    props.setContent([])
+  },[])
+  
+  const loadAiContent = async () =>
+  {
+    setAiCall(<CircularProgress/>)
+    await new Promise(resolve => setTimeout(resolve,1000))
+    //Set time delay to simulate loading
+    
+    props.setContent(
+      [
+        {
+          name: "ChatGPT",
+          description: "ChatGPT is a language model that can be used to generate text"
+        },
+        {
+          name: "Image Recognition",
+          description: "Use Computer Vision to recognize objects in images"
+        },
+        {
+          name: "Natural Language Processing",
+          description: "Use Natural Language Processing to understand and generate text"
+        }
+      ]
+    )
+    setAiCall(<AIButton/>)
+  }
+  const AIButton = () =>
+  {
+    return(
+      <Button onClick = {loadAiContent}>
+        <Typography variant="h6">Learn From ChatGPT what you can make</Typography>
+      </Button>
+    )
+  }
+  const [aiCall,setAiCall] = React.useState(<AIButton/>)
+  return (
+    <Modal
+      open={props.open}
+      onClose={props.onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Paper p={5} elevation={5} sx ={{
+        position: 'absolute',
+        width: "50%",
+        maxHeight: "70%", // Maximum height of the Paper component
+        overflow: "auto", // Enables scrolling for overflow content
+        backgroundColor: "#424242",
+        borderRadius: 4,
+        border: `2px solid ${themeOptions.palette.secondary.main}`,
+        boxShadow: 24,
+        p: 10,
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}>
+        <IconButton
+          aria-label="close"
+          onClick={props.onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Typography id="modal-modal-title" variant="h4" component="h2">
+          What can you do with {props.title}?
+        </Typography>
+        <Divider color="secondary" sx={{width:"100%"}}/>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <br/>
+            <Typography variant="h4">Use Cases</Typography>
+            <Divider color="orange" sx={{width:"100%"}}/>
+          </Grid>
+          <Grid item xs={12}>
+            {
+              props.content.length===0 ? aiCall : props.content.map((useCase,index)=>
+              {
+                return(
+                  <Typography variant="h5">{useCase.name} - {useCase.description}</Typography>
+                )
+              })
+            }
+          </Grid>
+          <Grid item xs={12}>
+            <br/>
+            <br/>
+            <Typography variant="h4">External Resources</Typography>
+            <Divider color="orange" sx={{width:"100%"}}/>
+          </Grid>
+          <Grid item xs={12}>
+            {props.links.map((link,index)=>
+            {
+              return(
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography variant="h6">{link.purpose}</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <QRCode size={128} value={link.hyperlink}/>
+                  </Grid>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </Grid>
+      </Paper>
+    </Modal>
+  );
+}
 
 function App() {
+  const [title,setTitle] = React.useState("");
+  const [links,setLinks] = React.useState([{}]);
+  const [content,setContent] = React.useState(
+    [
+    ]
+  )
+  const [loading,setLoading ] = React.useState(false);
+  const [search,setSearch] = React.useState("");
+  const [open,setOpen] = React.useState(false);
+  const handleClose = () =>{
+    setContent([])
+    setOpen(false);
+  }
+  const theme = createTheme(themeOptions)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <MoreInformationModal content={content} setContent={setContent} links = {links} title={title} open={open} onClose={handleClose}/>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h4" component="div" sx={{ }}>
+          Careers in Computer Science
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Paper sx={{width:"100%", height:"200vh"}}>
+      <Grid container spacing = {2}>
+        {roles.map((role,index)=>
+        {return(
+          <Grid item xs={12}>
+          <Accordion sx={{backgroundColor:"#424242"}}>
+            <AccordionSummary expandIcon={<ExpandMore/>}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Typography variant="h5">{role.title}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Divider color="orange" sx={{ width: "100%" }} orientation="horizontal"/>
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Button onClick={()=>
+                {
+                  setOpen(true)
+                  console.log(role.links)
+                  setLinks(role.links)
+                  setTitle(role.title)
+                  }}>
+                <Typography variant="h6">What can you make with {role.title}?</Typography>
+              </Button>
+              
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+        )
+        })}
+      </Grid>
+    <footer>Created by Isaiah Freeman</footer>
+    </Paper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/rolesData.js
@@ -0,0 +1,181 @@
+
+
+const jobTitles = [
+    "Mobile Development",
+    "Security Analysis",
+    "Frontend Development",
+    "Backend Development",
+    "Game Design",
+    "Ethical Hacking",
+    "Computer Vision",
+    "Natural Language Processing",
+    "Reinforcement Learning",
+    "IT Consulting",
+    "Project Management",
+    "Scrum",
+    "Solutions Architect",
+    "Blockchain",
+  ]
+
+
+export const roles = [
+    {
+      title: "Mobile Development",
+      links: [
+        {
+          purpose: "iOS - Basic",
+          hyperlink: "https://youtu.be/UNH0bE4zPtY?si=_1aGVJQ9vd-AAwan"
+        },
+        {
+          purpose: "Android - Basic",
+          hyperlink: "https://www.youtube.com/watch?v=3gEh1UxjCR8"
+        },
+        {
+          purpose: "Cross Platform - Advanced",
+          hyperlink: "https://www.youtube.com/watch?v=yCB_QC_UAMQ"
+        }
+      ]
+    },
+    {
+      title: "Security Analysis",
+      links: [
+        {
+          purpose: "Ethical Hacking",
+          hyperlink: "https://www.youtube.com/watch?v=yFC8pb2TPdc&list=PLIhvC56v63IIJZRa3lzK6IeBQOH_VFjUQ"
+        },
+        {
+          purpose: "Penetration Testing",
+          hyperlink: "https://www.youtube.com/watch?v=8ucrQ6Tj2js"
+        }
+      ]
+    },
+    {
+      title: "Frontend Development",
+      links: [
+        {
+          purpose: "Creating a Website",
+          hyperlink: "https://www.youtube.com/watch?v=VegDvRLVlOI"
+        },
+        {
+          purpose: "React - Advanced",
+          hyperlink: "https://www.youtube.com/watch?v=SqcY0GlETPk"
+        }
+      ]
+    },
+    {
+      title: "Backend Development",
+      links: [
+        {
+          purpose: "Node.js",
+          hyperlink: "https://www.youtube.com/watch?v=TlB_eWDSMt4"
+        },
+        {
+          purpose: "Django",
+          hyperlink: "https://www.youtube.com/watch?v=rHux0gMZ3Eg"
+        },
+        {
+          purpose: "Flask",
+          hyperlink: "https://youtu.be/mqhxxeeTbu0?si=gYN-X5ytKYxPamDO"
+        }
+      ]
+    },
+    {
+      title: "Game Design",
+      links: [
+        {
+          purpose: "Unity",
+          hyperlink: "https://www.youtube.com/watch?v=XtQMytORBmM"
+        },
+        {
+          purpose: "Unreal Engine",
+          hyperlink: "https://www.youtube.com/watch?v=LeY6tAP-qss"
+        },
+        {
+          purpose: "Godot",
+          hyperlink: "https://www.youtube.com/watch?v=42HKCFf5Lf4&list=PLhqJJNjsQ7KEcm-iYJ2a8UCRN62bTneKa"
+        }
+      ]
+    },
+    {
+        title:"Computer Vision",
+        links:[
+            {
+                purpose:"General Machine Learning - START HERE",
+                hyperlink:"https://www.youtube.com/watch?v=7eh4d6sabA0"
+            },
+            {
+                purpose: "Computer Vision - Beginner Projects",
+                hyperlink:"https://www.youtube.com/watch?v=aFNDh5k3SjU&list=PLb49csYFtO2Hpfn8eLnaD9tJ0xYcMVcWe"
+            }
+        ]
+    },
+    {
+        title:"Natural Language Processing",
+        links:[
+            
+            {
+                purpose:"General Machine Learning - START HERE",
+                hyperlink:"https://www.youtube.com/watch?v=7eh4d6sabA0"
+            },
+            {
+                purpose: "NLP - Beginner Projects",
+                hyperlink:"https://www.youtube.com/watch?v=hG8K5h2J-5g&list=PLdF3rLdF4ICQ4-fSEucMqoqMz1tEyjp9q"
+            }
+
+        ]
+    },
+    {
+        title:"Reinforcement Learning",
+        links:[
+            
+            {
+                purpose:"General Machine Learning - START HERE",
+                hyperlink:"https://www.youtube.com/watch?v=7eh4d6sabA0"
+            },
+            {
+                purpose: "Reinforcement Learning - Advanced",
+                hyperlink: "https://www.youtube.com/watch?v=nyjbcRQ-uQ8&list=PLZbbT5o_s2xoWNVdDudn51XM8lOuZ_Njv"
+            }
+        ]
+    },
+    {
+        title: "IT Support",
+        links:[
+            {
+                purpose: "Foundations of IT Support - Google",
+                hyperlink:"https://www.youtube.com/watch?v=lJC_sJ6jhDo&list=PLTZYG7bZ1u6pQJShZs9iV0aJNzsqTm4Mx"
+            }
+        ]
+    },
+    {
+        title:"Project Management",
+        links:[
+            {
+                purpose:" Google Project Management Certificate",
+                hyperlink:"https://www.youtube.com/watch?v=rck3MnC7OXA"
+            }
+        ]
+    },
+    {
+        title:"Solutions Architect",
+        links:[
+            {
+                purpose:"Solutions Architect",
+                hyperlink: "https://www.youtube.com/watch?v=FIQTPo359D8"
+            }
+        ]
+    },
+    {
+        title: "Blockchain",
+        links:[
+            {
+                purpose:"What is Blockchain?",
+                hyperlink:"https://www.youtube.com/watch?v=yubzJw0uiE4"
+            },
+            {
+                purpose: "Finding Blockchain Careers",
+                hyperlink:"https://www.youtube.com/watch?v=MZn1Li18n0I"
+            }
+        ]
+    }
+  ]
